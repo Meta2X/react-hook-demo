88f526d84760c4d9edc0f8c78528a8a87dc52d7f
MX <shady2doit@gmail.com>
2024-06-02 14:36:47 +0800

    useState示例

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css';
-import Demo1 from './components/demo1'
+
+
 function App() {
+  const [num, setNum] = useState(0)
+  const [num0, setNum0] = useState(()=>{
+    const num1 = 1+2+3+4+5
+    const num2 = 6+7+8+9+10
+    return num1+num2
+  })
   return (
     <div className="App">
-      react-hooks-demo
-      <Demo1 />
+      <button onClick={ () => setNum(num+1) }>{num}</button>
+
+      <button onClick={ () => setNum0((prev)=>prev+10) }>{num0}</button>
     </div>
   );
 }
