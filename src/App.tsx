7e14830d3a943234472e75b210e54ff88d65690d
赵林 <shady2pro@163.com>
2024-06-18 19:03:13 +0800

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
+import { useReducer } from 'react';
 import './App.css';
+type State =  {
+  result: number
+}
+type Action = {
+  type: 'add' | 'minus'
+  num: number
+}
+
+function reducer(state: State, action: Action){
+  switch(action.type){
+    case 'add':
+      return {
+        result: state.result + action.num
+      }
+    case 'minus':
+      return {
+        result: state.result - action.num
+      }
+    default:
+      return state
+  }
+}
 
 function App() {
+
+  const [res, dispatch] =  useReducer(reducer, {result: 0})
   return (
     <div className="App">
-      react-hooks-demo
+      {res.result}
+      <button onClick={()=>dispatch({type: 'add', num: 10})}>加</button>
+      <button onClick={()=>dispatch({type: 'minus', num: 10})}>减</button>
     </div>
   );
 }
