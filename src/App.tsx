7c014c0ad316610b0ed348ff4dc5c28ca00cc2ac
MX <shady2doit@gmail.com>
2024-06-19 10:36:49 +0800

    修改useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
+import { produce } from 'immer'
 import './App.css';
 type State =  {
   result: number
@@ -11,13 +12,17 @@ type Action = {
 function reducer(state: State, action: Action){
   switch(action.type){
     case 'add':
-      return {
-        result: state.result + action.num
-      }
+      return produce(state, (state)=>{
+        state.result = state.result + action.num
+      })
+      // immer 是依赖 Proxy 实现的，它会监听你在函数里对属性的修改，然后帮你创建一个新对象。
+      // {
+      //   result: state.result + action.num
+      // }
     case 'minus':
-      return {
-        result: state.result - action.num
-      }
+      return produce(state, (state)=>{
+        state.result = state.result - action.num
+      })
     default:
       return state
   }
@@ -26,11 +31,25 @@ function reducer(state: State, action: Action){
 function App() {
 
   const [res, dispatch] =  useReducer(reducer, {result: 0})
+  const [total, setTotal]= useState({num:0})
   return (
     <div className="App">
       {res.result}
       <button onClick={()=>dispatch({type: 'add', num: 10})}>加</button>
       <button onClick={()=>dispatch({type: 'minus', num: 10})}>减</button>
+      <div>-------</div>
+      {total.num}
+      <button onClick={()=>{
+        // total.num++;
+        // setTotal(total) 无效
+
+        // setTotal({num: total.num++}) 有效
+
+        setTotal(produce(total, (total)=>{ total.num++ })) // immer
+
+        // 在 react 里，只要涉及到 state 的修改，就必须返回新的对象，不管是 useState 还是 useReducer
+
+      }}>只加一</button>
     </div>
   );
 }
